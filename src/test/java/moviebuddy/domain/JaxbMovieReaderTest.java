ef1517b4bcdd3ce8e5a4d238fee3d859ab141b79
MeteorLee <meteorproject529@gmail.com>
2023-07-23 17:16:12 +0900

    refactor+test: 스프링 통합테스트로 테스트 코드 변경

--- src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
+++ src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
@@ -1,21 +1,24 @@
 package moviebuddy.domain;
 
+import moviebuddy.MovieBuddyFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.List;
 
+@SpringJUnitConfig(MovieBuddyFactory.class)
 class JaxbMovieReaderTest {
+
+    @Autowired
+    JaxbMovieReader movieReader;
 
     @Test
     void NotEmpty_LoadedMovies() {
-
-        JaxbMovieReader movieReader = new JaxbMovieReader();
-
         List<Movie> movies = movieReader.loadMovies();
         Assertions.assertEquals(1375, movies.size());
-//        MovieFinderTest.assertEquals(1375, movies.size());
-
     }
 
 }
