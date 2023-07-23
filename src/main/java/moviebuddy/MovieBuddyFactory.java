9b928e9e4be805931c9420b576b6efe181c429aa
MeteorLee <meteorproject529@gmail.com>
2023-07-23 16:05:14 +0900

    refactor: 빈 구성정보를 하나의 클래스에 여러 개 만들어서 관리

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -3,24 +3,30 @@ package moviebuddy;
 import moviebuddy.domain.CsvMovieReader;
 import moviebuddy.domain.MovieFinder;
 import moviebuddy.domain.MovieReader;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
+@Import({MovieBuddyFactory.DomainModuleConfig.class, MovieBuddyFactory.DataSourceModuleConfig.class})
+//@ImportResource("xml file location") xml로 작성한 경우
 public class MovieBuddyFactory {
 
-    @Bean
-    public MovieReader movieReader() {
-        return new CsvMovieReader();
+    @Configuration
+    static class DomainModuleConfig {
+        @Bean
+        public MovieFinder movieFinder(MovieReader movieReader) {
+            return new MovieFinder(movieReader);
+        }
     }
 
-    @Bean
-//    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-//    @Scope("prototype")
-    public MovieFinder movieFinder(MovieReader movieReader) {
-        return new MovieFinder(movieReader);
+    @Configuration
+    static class DataSourceModuleConfig {
+        @Bean
+        public MovieReader movieReader() {
+            return new CsvMovieReader();
+        }
     }
 
 }
