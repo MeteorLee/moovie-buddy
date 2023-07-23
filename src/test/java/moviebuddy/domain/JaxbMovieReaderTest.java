5cb5b7f31de9edc60783bddf99676fd3ea8a5316
MeteorLee <meteorproject529@gmail.com>
2023-07-23 10:42:20 +0900

    feat: xml문서 영화 목록 데이터로 변환 기능 추가

---
+++ src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
@@ -0,0 +1,16 @@
+package moviebuddy.domain;
+
+import java.util.List;
+
+class JaxbMovieReaderTest {
+
+    public static void main(String[] args) {
+
+        JaxbMovieReader movieReader = new JaxbMovieReader();
+
+        List<Movie> movies = movieReader.loadMovies();
+        MovieFinderTest.assertEquals(1375, movies.size());
+
+    }
+
+}
