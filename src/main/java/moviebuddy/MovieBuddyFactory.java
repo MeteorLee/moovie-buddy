4041fe81a71e86eb71d21cb4a26317b0c26d0f1e
MeteorLee <meteorproject529@gmail.com>
2023-07-24 18:03:56 +0900

    refactor: @value 어노테이션을 사용하여 환경 정보 설정 로직 변경

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -33,32 +33,6 @@ public class MovieBuddyFactory {
     @Configuration
     static class DataSourceModuleConfig {
 
-        private final Environment environment;
-
-        @Autowired
-        public DataSourceModuleConfig(Environment environment) {
-            this.environment = environment;
-        }
-
-        @Profile(MovieBuddyProfile.CSV_MODE)
-        @Bean
-        public CsvMovieReader csvMovieReader() {
-            CsvMovieReader csvMovieReader = new CsvMovieReader();
-
-            // 애플리케이션 외부에서 작성된 설정 정보를 읽어, 메타데이터 위치 설정하기
-            csvMovieReader.setMetadata(environment.getProperty("movie.metadata"));
-
-            return csvMovieReader;
-        }
-
-        @Profile(MovieBuddyProfile.XML_MODE)
-        @Bean
-        public XmlMovieReader xmlMovieReader(Unmarshaller unmarshaller) {
-            XmlMovieReader xmlMovieReader = new XmlMovieReader(unmarshaller);
-            xmlMovieReader.setMetadata(environment.getProperty("movie.metadata"));
-
-            return xmlMovieReader;
-        }
     }
 
 }
