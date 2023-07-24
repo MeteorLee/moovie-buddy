49c9952a0caf276e5b5ad34c4428c953ca91d6d7
MeteorLee <meteorproject529@gmail.com>
2023-07-24 17:58:04 +0900

    refactor: 스프링의 Environment를 이용해서 환경 설정 정보 얻도록 변경

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -2,7 +2,9 @@ package moviebuddy;
 
 import moviebuddy.data.CsvMovieReader;
 import moviebuddy.data.XmlMovieReader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.oxm.Unmarshaller;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
@@ -31,13 +33,20 @@ public class MovieBuddyFactory {
     @Configuration
     static class DataSourceModuleConfig {
 
+        private final Environment environment;
+
+        @Autowired
+        public DataSourceModuleConfig(Environment environment) {
+            this.environment = environment;
+        }
+
         @Profile(MovieBuddyProfile.CSV_MODE)
         @Bean
         public CsvMovieReader csvMovieReader() {
             CsvMovieReader csvMovieReader = new CsvMovieReader();
 
             // 애플리케이션 외부에서 작성된 설정 정보를 읽어, 메타데이터 위치 설정하기
-            csvMovieReader.setMetadata(System.getProperty("movie.metadata"));
+            csvMovieReader.setMetadata(environment.getProperty("movie.metadata"));
 
             return csvMovieReader;
         }
@@ -46,7 +55,7 @@ public class MovieBuddyFactory {
         @Bean
         public XmlMovieReader xmlMovieReader(Unmarshaller unmarshaller) {
             XmlMovieReader xmlMovieReader = new XmlMovieReader(unmarshaller);
-            xmlMovieReader.setMetadata(System.getProperty("movie.metadata"));
+            xmlMovieReader.setMetadata(environment.getProperty("movie.metadata"));
 
             return xmlMovieReader;
         }
