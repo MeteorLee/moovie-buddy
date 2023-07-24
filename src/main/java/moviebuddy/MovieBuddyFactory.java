d4a0f3151bf2de5047bd82f08e62592cec2d16cf
MeteorLee <meteorproject529@gmail.com>
2023-07-24 11:05:43 +0900

    refactor: 스프링 OXM서비스 추상화로 XML 데이터 읽기 로직 변경

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -1,12 +1,21 @@
 package moviebuddy;
 
 import org.springframework.context.annotation.*;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 @Configuration
 @ComponentScan(basePackages = {"moviebuddy"})
 @Import({MovieBuddyFactory.DomainModuleConfig.class, MovieBuddyFactory.DataSourceModuleConfig.class})
 //@ImportResource("xml file location") xml로 작성한 경우
 public class MovieBuddyFactory {
+
+    @Bean
+    public Jaxb2Marshaller jaxb2Marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setPackagesToScan("moviebuddy");
+
+        return marshaller;
+    }
 
     @Configuration
     static class DomainModuleConfig {
