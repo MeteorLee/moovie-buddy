25b3b1fd2012aa31a0baebc265f0da1f4e3efb7b
MeteorLee <meteorproject529@gmail.com>
2023-07-24 16:20:09 +0900

    refactor: 스프링 InitializingBean을 구현하여 빈 초기화 시 검증

--- src/main/java/moviebuddy/data/CsvMovieReader.java
+++ src/main/java/moviebuddy/data/CsvMovieReader.java
@@ -5,6 +5,7 @@ import moviebuddy.MovieBuddyProfile;
 import moviebuddy.domain.Movie;
 import moviebuddy.domain.MovieReader;
 import moviebuddy.util.FileSystemUtils;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
@@ -25,7 +26,7 @@ import java.util.stream.Collectors;
 
 @Profile(MovieBuddyProfile.CSV_MODE)
 @Repository
-public class CsvMovieReader implements MovieReader {
+public class CsvMovieReader implements MovieReader, InitializingBean {
 
 
     private String metadata;
@@ -34,16 +35,7 @@ public class CsvMovieReader implements MovieReader {
         return metadata;
     }
 
-    public void setMetadata(String metadata) throws FileNotFoundException, URISyntaxException {
-        URL metadataUrl = ClassLoader.getSystemResource(metadata);
-        if (Objects.isNull(metadataUrl)) {
-            throw new FileNotFoundException(metadata);
-        }
-
-        if (Files.isReadable(Path.of(metadataUrl.toURI())) == false) {
-            throw new ApplicationException(String.format("cannot read to metadata.[%s]", metadata));
-        }
-
+    public void setMetadata(String metadata) {
         this.metadata = metadata;
     }
 
@@ -86,5 +78,19 @@ public class CsvMovieReader implements MovieReader {
         } catch (IOException | URISyntaxException error) {
             throw new ApplicationException("failed to load movies data.", error);
         }
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+        URL metadataUrl = ClassLoader.getSystemResource(metadata);
+        if (Objects.isNull(metadataUrl)) {
+            throw new FileNotFoundException(metadata);
+        }
+
+        if (Files.isReadable(Path.of(metadataUrl.toURI())) == false) {
+            throw new ApplicationException(String.format("cannot read to metadata.[%s]", metadata));
+        }
+
     }
 }
