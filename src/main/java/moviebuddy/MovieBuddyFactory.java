eff56c4594853556653b14d929a42643dd81894c
MeteorLee <meteorproject529@gmail.com>
2023-08-05 16:56:09 +0900

    feat: 스프링의 프록시 팩토리 빈을 이용하여 캐시 기능을 프록시로 처리

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -2,12 +2,15 @@ package moviebuddy;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import moviebuddy.cache.CachingAdvice;
 import moviebuddy.data.CachingMovieReader;
 import moviebuddy.data.CsvMovieReader;
 import moviebuddy.domain.Movie;
 import moviebuddy.domain.MovieReader;
+import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
@@ -47,8 +50,15 @@ public class MovieBuddyFactory {
 
         @Primary
         @Bean
-        public MovieReader cachingMovieReader(CacheManager cacheManager, MovieReader target) {
-            return new CachingMovieReader(cacheManager, target);
+        public ProxyFactoryBean cachingMovieReaderFactory(ApplicationContext applicationContext) {
+            MovieReader target = applicationContext.getBean(MovieReader.class);
+            CacheManager cacheManager = applicationContext.getBean(CacheManager.class);
+
+            ProxyFactoryBean proxyFactoryBean = new ProxyFactoryBean();
+            proxyFactoryBean.setTarget(target);
+            proxyFactoryBean.addAdvice(new CachingAdvice(cacheManager));
+
+            return proxyFactoryBean;
         }
 
     }
