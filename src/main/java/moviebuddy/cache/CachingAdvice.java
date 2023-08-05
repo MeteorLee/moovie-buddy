a871cd937f7420f9e51afeee9b94e83d407ebeae
MeteorLee <meteorproject529@gmail.com>
2023-08-05 17:35:59 +0900

    refactor: 대상 객체의 대상 메서드에만 프록시가 작동하도록 구현(주석 처리)
    하나의 클래스에 2개의 관심사(대상 객체 정보 검증, 캐싱)가 있으므로 좋지 않은 코드

--- src/main/java/moviebuddy/cache/CachingAdvice.java
+++ src/main/java/moviebuddy/cache/CachingAdvice.java
@@ -1,15 +1,20 @@
 package moviebuddy.cache;
 
+import moviebuddy.domain.MovieReader;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+import org.springframework.util.ClassUtils;
 
 import java.util.Objects;
 
 public class CachingAdvice implements MethodInterceptor {
 
-    CacheManager cacheManager;
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private final CacheManager cacheManager;
 
     public CachingAdvice(CacheManager cacheManager) {
         this.cacheManager = Objects.requireNonNull(cacheManager);
@@ -17,16 +22,30 @@ public class CachingAdvice implements MethodInterceptor {
 
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
+        /*// 부가 기능을 적용할 대상 객체나 메서드를 판별하기 
+        // 대상 객체가 MovieReader 타입이 아니면 즉시 반환하기
+        if (!ClassUtils.isAssignableValue(MovieReader.class, invocation.getThis())) { 
+            return invocation.proceed();
+        }
+
+        // 메서드 이름이 loadMovies가 아니면 즉시 반환하기
+        if (!"loadMovies".equals(invocation.getMethod().getName())) {
+            return invocation.proceed();
+        } -> 관심사의 분리를 지키지 않음
+        */
+
         // 캐시된 데이터가 존재하면, 즉시 반환
         Cache cache = cacheManager.getCache(invocation.getThis().getClass().getName());
         Object cachedValue = cache.get(invocation.getMethod().getName(), Object.class);
         if (Objects.nonNull(cachedValue)) {
+            log.info("returns cached data. [{}]", invocation);
             return cachedValue;
         }
 
         // 캐시된 데이터가 없으면, 대상 객체에 명령을 위임하고, 반환된 값을 캐시에 저장 후 반환 처리
         cachedValue = invocation.proceed();
         cache.put(invocation.getMethod().getName(), cachedValue);
+        log.info("caching return value. [{}]", invocation);
 
         return cachedValue;
     }
